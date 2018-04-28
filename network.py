28c4dfb8c485aa43ac406c0854c2610b261ec989
Alex Golts <alexgbiz@gmail.com>
2018-04-28 17:40:05 +0000

    encoder

--- network.py
+++ network.py
@@ -9,14 +9,34 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
 
-        self.conv = nn.Conv2d(
-            3, 64, kernel_size=3, stride=2, padding=1, bias=False)
-        # TODO...
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=3)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(96, 128, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(2)
+        self.conv4 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool4 = nn.MaxPool2d(2)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool5 = nn.MaxPool2d(2)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool6 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(512,1024)
 
     def forward(self, input, hidden1, hidden2, hidden3):
-        x = self.conv(input)
-        # TODO...
-
+        x = self.conv1(input)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)        
+        x = self.conv3(x)
+        x = self.pool3(x)
+        x = self.conv4(x)
+        x = self.pool4(x)
+        x = self.conv5(x)
+        x = self.pool5(x)
+        x = self.conv6(x)
+        x = self.pool6(x)
+        
         return x
 
 
